20901c4144010169bb48893a322d9470bd1d8c80
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-04 02:57:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ attr_reader :name, :backed_projects, :title
   
   def back_project(project)
     @backed_projects << project
-    Backers.backed_projects << self 
+    project.backers << self
   end 
   
 end 
